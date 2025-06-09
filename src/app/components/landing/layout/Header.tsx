d00c06a3e466f36cce1412f13521ca1ddfbb8adc
Anderson Kennedy <andersonkenneed@gmail.com>
2025-06-09 19:09:42 +0000

    O Rafaela Felix Lash & Brow ESTÁ MUITO COLADO A ESQUERA

--- src/app/components/landing/layout/Header.tsx
+++ src/app/components/landing/layout/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 max-w-screen-2xl items-center justify-between">
+      <div className="container px-4 flex h-16 max-w-screen-2xl items-center justify-between">
         <Link href="/" className="mr-6 flex items-center space-x-2">
           <span className="font-headline text-xl md:text-2xl font-bold text-accent">Rafaela Felix Lash & Brow</span>
         </Link>
